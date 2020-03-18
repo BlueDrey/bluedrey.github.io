af84dda91e749d4278b39267a880d8811c017459
BlueDrey <andreas.zaidan@gmail.com>
2020-03-18 19:26:47 +0100

    Initial commit

---
+++ precache-manifest.10c1c68d3d77d7e6ec16cead34c96b22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "290c02b1ecf169b6009b04591b415832",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1ea85e14d15e5d0c835",
+    "url": "/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "4b49881c9a3347ad7f02",
+    "url": "/static/css/main.f411739e.chunk.css"
+  },
+  {
+    "revision": "c1ea85e14d15e5d0c835",
+    "url": "/static/js/2.bcb2ca04.chunk.js"
+  },
+  {
+    "revision": "4a5059cf42ad3c48d6028f0a3937bbeb",
+    "url": "/static/js/2.bcb2ca04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b49881c9a3347ad7f02",
+    "url": "/static/js/main.9ab2d5bd.chunk.js"
+  },
+  {
+    "revision": "7b70fc4ba8e5ebcbd7d2",
+    "url": "/static/js/runtime-main.c2cfd4da.js"
+  }
+]);
